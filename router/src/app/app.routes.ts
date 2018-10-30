cac52542d6889d67c2104d65dba965ca34e89dc1
dperezarbues <32041839+dperezarbues@users.noreply.github.com>
2018-10-30 15:50:34 +0100

    router

--- router/src/app/app.routes.ts
+++ router/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { AComponent } from "./a/a.component";
 import { BComponent } from "./b/b.component";
 import { CComponent } from "./c/c.component";
+import {Routes} from "@angular/router";
 
-export const routes = [
+export const routes:Routes = [
     {path:'', redirectTo:'/A', pathMatch:'full'},
     {path:'A', component: AComponent},
     {path:'B', component: BComponent},
