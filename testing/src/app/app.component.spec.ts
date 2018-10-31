7e0ec837bcff618a8ec3723dfc70287d1717ce68
dperezarbues <32041839+dperezarbues@users.noreply.github.com>
2018-10-31 12:39:23 +0100

    Testing

---
+++ testing/src/app/app.component.spec.ts
@@ -0,0 +1,51 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'testing'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('testing');
+  });
+
+  it(`should have as title 'testing'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.motto).toEqual('testing rulez');
+  });
+
+  it('should render title in a h1 tag', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to testing!');
+  });
+
+  it('should render motto in a p tag', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).toContain('testing rulez');
+  });
+
+  it('should only have 2 p tags', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelectorAll('p').length).toBe(2);
+  });
+});
---
+++ testing/src/app/pruebaTest1.spec.ts
@@ -0,0 +1,4 @@
+
+describe ('TEst de prueba 1', () => {
+    it('true is true', ()=> expect(true).toBe(true));
+});
